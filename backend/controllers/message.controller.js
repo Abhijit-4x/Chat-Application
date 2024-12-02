536a2b55f1afae5f16c60ced7511a6be86f37fbd
Abhijit <abhijitrs1928@gmail.com>
2024-12-02 14:08:21 +0530

    first commit - backend

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,66 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    // socket io functionality will come here
+
+    // this runs one-by-one, hence takes more time
+    // await conversation.save();
+    // await newMessage.save();
+
+    // this runs all the process in parallel
+    await Promise.all([await conversation.save(), await newMessage.save()]);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // populate is a mongoose property, that allows us to fetch the object from messages collection, using the stored object id
+
+    if(!conversation){
+        return res.status(200).json([]);
+    }
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+
+  } catch (error) {
+    console.log("Error in getMessage Controller", error.message);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDb.js";
+// const express = require('express'); - to avoid importing in this manner we added "type":"module" in package.json file
+// const dotenv = require('dotenv');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  // root path - https://localhost:5000/
+  res.send("Server running!!");
+});
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}.`);
+});
